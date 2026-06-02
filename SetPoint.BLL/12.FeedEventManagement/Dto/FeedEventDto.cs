2fadc24645bd17d0214ede5a1804ba606daabce8
Piotrek Budzowski <piobudzows@gmail.com>
2026-06-02 17:42:40 +0200

    FeedEvent: implement prodcuts data trasfer object (dto)

---
+++ SetPoint.BLL/12.FeedEventManagement/Dto/FeedEventDto.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SetPoint.BLL._12.FeedEventManagement.Dto
+{
+    public class FeedEventDto
+    {
+        [JsonPropertyName("ID")]
+        public Guid Id { get; set; }
+
+        [JsonPropertyName("CREATED_AT")]
+        public DateTime? CreatedAt { get; set; }
+
+        [JsonPropertyName("UPDATED_AT")]
+        public DateTime? UpdatedAt { get; set; }
+
+        [JsonPropertyName("DELETED_AT")]
+        public DateTime? DeletedAt { get; set; }
+
+        [JsonPropertyName("USER_ID")]
+        public Guid UserId { get; set; } = Guid.Empty;
+
+        [JsonPropertyName("EVENT_TYPE")]
+        public FeedEventType EventType { get; set; }
+
+        [JsonPropertyName("EXERCISE_ID")]
+        public Guid? ExerciseId { get; set; } = null;
+
+        [JsonPropertyName("METRIC_VALUE")]
+        public double? MetricValue { get; set; } = null;
+
+        [JsonPropertyName("CUSTOM_TEXT")]
+        public string? CustomText { get; set; } = null;
+    }
+}
