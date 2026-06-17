a55e12b8d8cac7decb29d148d5c8dbd04a9ce10b
Piotrek Budzowski <piobudzows@gmail.com>
2026-06-17 23:46:48 +0200

    feat(email): implement EmailService and IEmailService interface

---
+++ SetPoint.BLL/0.Security/EmailService.cs
@@ -0,0 +1,72 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MimeKit.Text;
+
+namespace SetPoint.BLL._0.Security
+{
+    public class EmailService : IEmailService
+    {
+        #region Fields
+        private readonly IConfiguration _config;
+        private readonly string _smtpServer;
+        private readonly int _smtpPort;
+        private readonly string _senderEmail;
+        private readonly string _senderPassword;
+
+        #endregion
+
+
+        #region Constructors
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+
+            _smtpServer = _config["EmailSettings:Host"]
+                ?? throw new InvalidOperationException("Email Host not found in configuration.");
+
+            _smtpPort = int.TryParse(_config["EmailSettings:Port"], out var port) ? port : 587;
+
+            _senderEmail = _config["EmailSettings:Username"]
+                ?? throw new InvalidOperationException("Email Username not found in configuration.");
+
+            _senderPassword = _config["EmailSettings:Password"]
+                ?? throw new InvalidOperationException("Email Password not found in configuration.");
+        }
+        #endregion
+
+
+        #region Methods
+        public async Task<bool> SendEmailAsync(string toEmail, string subject, string htmlBody)
+        {
+            var email = new MimeMessage();
+
+            email.From.Add(new MailboxAddress("SetPoint App", _senderEmail));
+
+            email.To.Add(MailboxAddress.Parse(toEmail));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html)
+            {
+                Text = htmlBody
+            };
+
+            using (var smtp = new SmtpClient())
+            {
+                try
+                {
+                    await smtp.ConnectAsync(_smtpServer, _smtpPort, SecureSocketOptions.StartTls);
+                    await smtp.AuthenticateAsync(_senderEmail, _senderPassword);
+                    await smtp.SendAsync(email);
+                    await smtp.DisconnectAsync(true);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ SetPoint.BLL/0.Security/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace SetPoint.BLL._0.Security
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmailAsync(string toEmail, string subject, string htmlBody);
+    }
+}
