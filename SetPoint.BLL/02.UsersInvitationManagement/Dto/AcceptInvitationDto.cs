4438277c1a43e0223f76d17fa6f760c5bc451d8c
Piotrek Budzowski <piobudzows@gmail.com>
2026-06-23 13:10:45 +0200

    feat(user-invitation): implement AcceptInvitationDto

---
+++ SetPoint.BLL/02.UsersInvitationManagement/Dto/AcceptInvitationDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SetPoint.BLL._02.UsersInvitationManagement.Dto
+{
+    public class AcceptInvitationDto
+    {
+        [JsonPropertyName("Token")]
+        public required Guid Token { get; set; }
+
+        [JsonPropertyName("Email")]
+        public required string FullName { get; set; }
+
+        [JsonPropertyName("Password")]
+        public required string Password { get; set; }
+    }
+}
