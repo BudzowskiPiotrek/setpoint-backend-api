8dda83d06573c272942a02ca1c1c7d10687b6d2e
Piotrek Budzowski <piobudzows@gmail.com>
2026-07-02 09:43:45 +0200

    feat(user): add TermsAcceptedAt field and its migration

---
+++ SetPoint.DAL/Migrations/20260702073525_AddTermsAcceptedAtToUsers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SetPoint.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTermsAcceptedAtToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TERMS_ACCEPTED_AT",
+                table: "USERS",
+                type: "timestamp with time zone",
+                nullable: true)
+                .Annotation("Relational:ColumnOrder", 12);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TERMS_ACCEPTED_AT",
+                table: "USERS");
+        }
+    }
+}
