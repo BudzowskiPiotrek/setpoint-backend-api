15fc81b9366ce0fbef73a6821fe7992c37985825
Piotrek Budzowski <piobudzows@gmail.com>
2026-08-11 09:49:58 +0200

    test(sync-controller): scaffold unit test structure for push and pull endpoints

---
+++ SetPoint.API.Tests/Controllers/SyncControllerTests.cs
@@ -0,0 +1,66 @@
+﻿namespace SetPoint.Api.Tests.Controllers
+{
+    public class SyncControllerTests
+    {
+
+
+
+
+
+
+
+        #region Push
+        [Fact(Skip = "TODO: yet to be developed")]
+        public async Task SyncController_Push_WhenModelStateInvalid_Returns400()
+        {
+        }
+
+        [Fact(Skip = "TODO: yet to be developed")]
+        public async Task SyncController_Push_WhenUserIdClaimMissing_Returns401()
+        {
+        }
+
+        [Fact(Skip = "TODO: yet to be developed")]
+        public async Task SyncController_Push_WhenUserIdClaimInvalidGuid_Returns401()
+        {
+        }
+
+        [Fact(Skip = "TODO: yet to be developed")]
+        public async Task SyncController_Push_WhenBllReturnsResult_Returns200()
+        {
+        }
+
+        [Fact(Skip = "TODO: yet to be developed")]
+        public async Task SyncController_Push_WhenBllThrowsException_Returns500()
+        {
+        }
+        #endregion
+
+        #region Pull
+        [Fact(Skip = "TODO: yet to be developed")]
+        public async Task SyncController_Pull_WhenModelStateInvalid_Returns400()
+        {
+        }
+
+        [Fact(Skip = "TODO: yet to be developed")]
+        public async Task SyncController_Pull_WhenUserIdClaimMissing_Returns401()
+        {
+        }
+
+        [Fact(Skip = "TODO: yet to be developed")]
+        public async Task SyncController_Pull_WhenUserIdClaimInvalidGuid_Returns401()
+        {
+        }
+
+        [Fact(Skip = "TODO: yet to be developed")]
+        public async Task SyncController_Pull_WhenBllReturnsResult_Returns200()
+        {
+        }
+
+        [Fact(Skip = "TODO: yet to be developed")]
+        public async Task SyncController_Pull_WhenBllThrowsException_Returns500()
+        {
+        }
+        #endregion
+    }
+}
